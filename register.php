37e312df1fd26d3fdb8acb025c63b180b00551b8
Abhiram Pai <abhirampai1999@gmail.com>
2020-05-16 10:58:26 +0530

    Update register.php

--- register.php
+++ register.php
@@ -21,6 +21,9 @@
 <head>
 	<title>Welcome to Geenth!</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/register.css">
+	<link rel="icon" 
+      type="image/png" 
+	    href="https://www.google.com/imgres?imgurl=https%3A%2F%2Flibrary.kissclipart.com%2F20200103%2Fuyw%2Fkissclipart-music-jazz-musical-instrument-musician-saxophone-c8addd107cf45336.png&imgrefurl=https%3A%2F%2Fwww.kissclipart.com%2Fmusic-jazz-musical-instrument-musician-saxophone-z0dgay%2F&tbnid=WK1As5N7iLcC0M&vet=12ahUKEwjhvsWvz7fpAhWTCLcAHRbADMEQMyg1egQIARB-..i&docid=nRIjeg511khnVM&w=900&h=900&q=favicon%20for%20music&hl=en&ved=2ahUKEwjhvsWvz7fpAhWTCLcAHRbADMEQMyg1egQIARB-">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"
 	></script>
 	<script src="assets/script/register.js"></script> 
